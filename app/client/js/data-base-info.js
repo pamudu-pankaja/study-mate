dfc53f4d1e53cc9467a4660f3f8a849f01341327
mAster <podda2543@gmail.com>
2025-05-13 17:09:20 +0530

    vector_index_input DONE

---
+++ app/client/js/data-base-info.js
@@ -0,0 +1,12 @@
+function setIndex(){
+    const indexName = document.getElementById('index').value;
+
+    if (indexName.trim() == "" ) {
+        alert("Please enter a valid index name.");
+        return;
+    }
+    else{
+        console.log(`Index submitted : ${indexName}`)
+    }    
+}
+

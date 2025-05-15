375f27e85e3cc85f4b3a1b883c55f0808c41a967
mAster <podda2543@gmail.com>
2025-05-15 15:06:39 +0530

    Add File input complete Front-end only

---
+++ app/static/js/import-file.js
@@ -0,0 +1,30 @@
+const input = document.getElementById('file_input');
+const display = document.getElementById('file_name');
+const fallBackDisplay =document.getElementById("file-fallback-message")
+
+input.addEventListener('change' ,() => {
+    const file = input.files[0];
+    const max_size = 1 * 1024 * 1024;
+    
+    if (!file) return;
+
+    if (file.type !== 'application/pdf') {
+        fallBackDisplay.textContent="Only PDF file are allowed !";
+        fallBackDisplay.classList.add("visible");
+        input.value = '';
+        display.textContent='No file chosen';
+        return;
+    }
+
+    if (file.size >= max_size){
+        fallBackDisplay.textContent="File must be under 1 MB !";
+        fallBackDisplay.classList.add("visible");
+        input.value = '';
+        display.textContent='No file chosen';
+        return;
+    }
+    
+    fallBackDisplay.classList.remove('visible')
+    display.textContent = file.name;
+    display.title = file.name;
+})
--- app/static/js/index-name.js
+++ app/static/js/index-name.js
@@ -1,11 +1,13 @@
 function setIndex(){
     const indexName = document.getElementById('index').value;
+    const fallBackDisplay = document.getElementById('index-fallback-message');
 
     if (indexName.trim() == "" ) {
-        alert("Please enter a valid index name.");
+        fallBackDisplay.classList.add('visible')       
         return;
     }
     else{
+        fallBackDisplay.classList.remove('visible')
         console.log(`Index submitted : ${indexName}`)
     }    
 }

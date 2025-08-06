67193f7d9ca5ba4776db1cda82febbe21fc6f4f5
mAster <podda2543@gmail.com>
2025-08-06 14:42:56 +0530

    static versioin manager

--- app/chatbot.py
+++ app/chatbot.py
@@ -22,6 +22,17 @@ context = None
 app.config["UPLOAD_FOLDER"] = "/tmp/uploads"
 
 
+@app.context_processor
+def override_url_for():
+    def dated_url_for(endpoint, **values):
+        if endpoint == 'static':
+            filename = values.get('filename', None)
+            if filename:
+                file_path = os.path.join(app.static_folder, filename)
+                if os.path.isfile(file_path):
+                    values['v'] = int(os.stat(file_path).st_mtime)
+        return url_for(endpoint, **values)
+    return dict(url_for=dated_url_for)
 
 @app.route("/", methods=["GET"])
 def home():
